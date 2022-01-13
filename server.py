fc379a1e13dc0962b93ab87ed3fa135153fbf439
Pavel Zakharov <pzakharov83@gmail.com>
2022-01-13 21:21:09 +0300

    added Dockerfile

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from aiohttp import web
 import aiofiles
+import argparse
 import datetime as dt
 import asyncio
+import os
+import logging
+from pathlib import Path
 
 
 INTERVAL_SECS = 1
@@ -9,51 +13,81 @@ CHUNK_SIZE = 100
 KILOBYTE = 1024
 READ_SIZE = CHUNK_SIZE * KILOBYTE # 100 KB
 
+format = '%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+# logging.basicConfig(level=logging.INFO, format=format)
+# log = logging.getLogger(__name__)
 
+log_levels = {
+        'critical': logging.CRITICAL,
+        'error': logging.ERROR,
+        'warn': logging.WARNING,
+        'warning': logging.WARNING,
+        'info': logging.INFO,
+        'debug': logging.DEBUG,
+        '': logging.NOTSET
+}
 
-async def uptime_handle(request):
-
-    response = web.StreamResponse()
-
-    response.headers['Content-Type'] = 'text/html'
-
-    await response.prepare(request)
-
-    while True:
-        formatted_date = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        message = f'{formatted_date}<br>'
-
-        await response.write(message.encode('utf-8'))
-
-        await asyncio.sleep(INTERVAL_SECS)
+parser = argparse.ArgumentParser(description='Serve files from folders as zip archive')
+parser.add_argument('--loglevel', default='', choices=log_levels, type=str.lower)
+parser.add_argument('--delay', default=1, type=int)
+parser.add_argument('--path')
+main_args = parser.parse_args()
+logging.basicConfig(level=log_levels[main_args.loglevel], format=format)
+log = logging.getLogger(__name__)
 
 async def archivate(request):
-    path = request.match_info['archive_hash']
+    if main_args.path:
+        path = Path(main_args.path) / request.match_info['archive_hash']
+    else:
+        path = Path('test_photos') / request.match_info['archive_hash']
+    if '.' in str(path):
+        raise aiohttp.web.HTTPNotFound(body='Something goes wrong')
+    if not os.path.exists(path):
+        raise aiohttp.web.HTTPNotFound(body='File is deleted or never existed')
     args = ['-r', '-', '.', '-i', '*']
     proc = await asyncio.create_subprocess_exec(
-            'zip', *args, cwd=f'test_photos/{path}',
+            'zip', *args, cwd=path,
             stdout=asyncio.subprocess.PIPE
     )
-    cursor = 0
-    full_arch = b''
-    while True:
-        if not proc.stdout.at_eof():
-            data = await proc.stdout.read(n=READ_SIZE)
-        else:
-            break
-        full_arch += data
-        data_length = len(data)
-
-#    with open('smtharch.zip', 'w+b') as f1:
-#        f1.seek(cursor)
-#        f1.write(full_arch)
     response = web.StreamResponse()
-#    response.headers['Content-Type'] = 'text/html'
     response.headers['Content-Disposition'] = f'attachment; filename="{path}.zip"'
+    response.headers['Transfer-Encoding'] = 'deflate; chunked'
+    response.headers['Connection'] = 'keep-alive'
     await response.prepare(request)
-    await response.write(full_arch)
+    
+    try:
 
-#    raise NotImplementedError
+        while True:
+            if not proc.stdout.at_eof():
+                data = await proc.stdout.read(n=READ_SIZE)
+            else:
+                break
+            await asyncio.sleep(main_args.delay)
+            await response.write(data)
+
+            log.info('Sending archive chunk ...')
+    except asyncio.CancelledError as e:
+        log.debug('CancelledError class')
+        log.info('User canceled download')
+        proc.kill()
+        outs, errs = await proc.communicate()
+        raise
+    except Exception as e:
+        log.debug('Exception class')
+        log.info('Download was interrupted')
+        log.error(str(e))
+        proc.kill()
+        outs, errs = await proc.communicate()
+        raise
+    except BaseException as e:
+        log.debug('BaseException class')
+        log.error(str(e))
+        proc.kill()
+        outs, errs = await proc.communicate()
+    finally:
+        log.debug('Finally block')
+        log.info('Sending End of file')
+        await response.write_eof()
 
 
 async def handle_index_page(request):
@@ -66,7 +100,6 @@ if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-#        web.get('/', uptime_handle),
         web.get('/archive/{archive_hash}/', archivate),
     ])
     web.run_app(app)
