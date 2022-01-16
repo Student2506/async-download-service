fceb5645f719ea78a6430e0835634f86228f02e1
Pavel Zakharov <pzakharov83@gmail.com>
2022-01-16 12:50:22 +0300

    rewrite reqs.txt; changed error message; rewrote exception logic

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
-from aiohttp import web
-import aiofiles
 import argparse
-import datetime as dt
 import asyncio
-import os
 import logging
+import os
 from pathlib import Path
 
+import aiofiles
+from aiohttp import web
 
 INTERVAL_SECS = 1
 CHUNK_SIZE = 100
 KILOBYTE = 1024
-READ_SIZE = CHUNK_SIZE * KILOBYTE # 100 KB
+READ_SIZE = CHUNK_SIZE * KILOBYTE  # 100 KB
 
 format = '%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 
@@ -25,13 +24,18 @@ log_levels = {
         '': logging.NOTSET
 }
 
-parser = argparse.ArgumentParser(description='Serve files from folders as zip archive')
-parser.add_argument('--loglevel', default='', choices=log_levels, type=str.lower)
+parser = argparse.ArgumentParser(
+        description='Serve files from folders as zip archive'
+)
+parser.add_argument(
+        '--loglevel', default='', choices=log_levels, type=str.lower
+)
 parser.add_argument('--delay', default=1, type=int)
 parser.add_argument('--path')
 main_args = parser.parse_args()
 logging.basicConfig(level=log_levels[main_args.loglevel], format=format)
 log = logging.getLogger(__name__)
+
 
 async def archivate(request):
     if main_args.path:
@@ -39,53 +43,48 @@ async def archivate(request):
     else:
         path = Path('test_photos') / request.match_info['archive_hash']
     if '.' in str(path):
-        raise aiohttp.web.HTTPNotFound(body='Something goes wrong')
+        raise web.HTTPNotFound(body='Using dot "." is not allowed')
     if not os.path.exists(path):
-        raise aiohttp.web.HTTPNotFound(body='File is deleted or never existed')
+        raise web.HTTPNotFound(body='File is deleted or never existed')
     args = ['-r', '-', '.', '-i', '*']
     proc = await asyncio.create_subprocess_exec(
             'zip', *args, cwd=path,
             stdout=asyncio.subprocess.PIPE
     )
     response = web.StreamResponse()
-    response.headers['Content-Disposition'] = f'attachment; filename="{path}.zip"'
+    response.headers['Content-Disposition'] = (
+            f'attachment; filename="{path}.zip"'
+    )
     response.headers['Transfer-Encoding'] = 'deflate; chunked'
     response.headers['Connection'] = 'keep-alive'
     await response.prepare(request)
-    
-    try:
 
-        while True:
-            if not proc.stdout.at_eof():
-                data = await proc.stdout.read(n=READ_SIZE)
-            else:
-                break
+    try:
+        while not proc.stdout.at_eof():
+            data = await proc.stdout.read(n=READ_SIZE)
             await asyncio.sleep(main_args.delay)
             await response.write(data)
-
             log.info('Sending archive chunk ...')
-    except asyncio.CancelledError as e:
-        log.debug('CancelledError class')
+    except asyncio.CancelledError:
         log.info('User canceled download')
-        proc.kill()
-        outs, errs = await proc.communicate()
         raise
     except Exception as e:
-        log.debug('Exception class')
         log.info('Download was interrupted')
         log.error(str(e))
-        proc.kill()
-        outs, errs = await proc.communicate()
         raise
     except BaseException as e:
-        log.debug('BaseException class')
         log.error(str(e))
-        proc.kill()
-        outs, errs = await proc.communicate()
     finally:
-        log.debug('Finally block')
+        try:
+            proc.kill()
+            outs, errs = await proc.communicate()
+        except ProcessLookupError:
+            pass
         log.info('Sending End of file')
-        await response.write_eof()
+        try:
+            await response.write_eof()
+        except ConnectionResetError:
+            pass
 
 
 async def handle_index_page(request):
